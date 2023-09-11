c29b2bc8b4ed6a6c96b16d5541ab3f78cc8f88dc
Patricia Bongiovanni Catandi <62657143+patriciacatandi@users.noreply.github.com>
2023-09-11 20:03:40 -0300

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,10 @@
+def to_string(lista_ref):
+    """ Constrói uma string a partir dos elementos de uma lista
+
+    Args:
+        lista_ref (list): lista de referência para construção da string
+
+    Returns:
+        [str]: string formada pelos elementos da lista_ref
+    """
+    return ''.join(lista_ref)
